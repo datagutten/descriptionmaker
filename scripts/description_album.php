b375f1ef6ee142bebbe2800405072906f14b33bd
datagutten <datagutten@datagutten.net>
2020-10-31 14:41:35 +0100

    Code cleanup

--- scripts/description_album.php
+++ scripts/description_album.php
@@ -7,14 +7,10 @@ use datagutten\tools\files\files;
 use OrpheusNET\Logchecker\Logchecker;
 
 require __DIR__.'/../vendor/autoload.php';
-require '../src/EAClogBBCode.php';
 
 $mb=new MusicBrainzDescription;
-//$albuminfo=$mb->getrelease($argv[2]);
-//print_r($albuminfo);
 
 try {
-    //$metadata=$mb->metadata($argv[1]);
     $file = files::first_file($argv[1], ['flac']);
     $metadata = AudioMetadata::read_metadata($file);
     $desc = $mb->build_description($metadata);
@@ -40,13 +36,11 @@ else
         list($score, $details, $checksum_state, $log_text) = $logchecker->parse();
 
         $log_bbcode = EAClogBBCode::rewrite($log_text);
-        //if($checksum_state)
-        //Log checksum %s\n
         $desc .= sprintf("\n\nLog score: %d\n[spoiler]%s[/spoiler]", $score, $log_bbcode);
 
     }
-    //var_dump($file_log);
+
     file_put_contents($file_description,$desc); //Write the complete description to a file
-    printf("Tracklist saved as %s\n", $file_description);
+    printf("Description saved as %s\n", $file_description);
 }
 
