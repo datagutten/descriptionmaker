de2b03e03bc3e0eff4b85088cee55f6db22766fa
datagutten <datagutten@datagutten.net>
2021-10-31 08:43:41 +0100

    Move mediainfo to separate class

--- functions_description.php
+++ functions_description.php
@@ -2,15 +2,9 @@
 
 use datagutten\descriptionMaker\Snapshots;
 use datagutten\image_host;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
 
 class description
 {
-    /**
-     * @var dependcheck
-     */
-	private $dependcheck;
     /**
      * @var video
      */
@@ -36,81 +30,7 @@ class description
             $this->image_host = new $image_host($config['imagehost']);
             $this->snapshots = new Snapshots($this->image_host);
         }
-        $this->dependcheck = new dependcheck;
         $this->video = new video;
     }
 
-    /**
-     * @param string $file
-     * @return string
-     * @throws DependencyFailedException
-     * @throws FileNotFoundException
-     */
-	public function mediainfo($file)
-	{
-	    if(!file_exists($file))
-	        throw new FileNotFoundException($file);
-		$this->dependcheck->depend('mediainfo');
-
-        $process = new Process(array('mediainfo', '--Output=XML', $file));
-        $process->run();
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-
-		//$info=shell_exec("mediainfo --Output=XML \"$path\" 2>&1");
-		//die($info);
-		$xml=simplexml_load_string($process->getOutput());
-        $key_lengths = [];
-        $output = [];
-		foreach ($xml->{'media'}->{'track'} as $track)
-		{
-            $output[] = $track->attributes()->{'type'};
-			//$output[]=$data['@attributes']['type'];
-			$outputkeys[]='header';
-			foreach ($track as $key=>$value) {
-                if (array_search($key, array('@attributes', 'Unique_ID', 'Complete_name', 'Encoding_settings', 'Color_primaries', 'Transfer_characteristics', 'Matrix_coefficients')) === false) {
-                    $output[] = $value;
-                    $outputkeys[] = $key;
-                    $key_lengths[] = strlen($key);
-                }
-            }
-		}
-
-		$maxlen=max($key_lengths); //Find the longest key
-		$mediainfo='';
-		foreach ($output as $key=>$value)
-		{
-			if ($outputkeys[$key]!='header')
-				$mediainfo.=str_pad($outputkeys[$key],$maxlen+5).": $value\n";
-			else
-				$mediainfo.= "\n[b]".$value."[/b]\n";	
-		}
-		return $mediainfo;
-	}
-
-    /**
-     * @param string $file
-     * @return string
-     * @throws DependencyFailedException
-     * @throws FileNotFoundException
-     */
-	public static function simplemediainfo(string $file): string
-	{
-	    $dependcheck = new dependcheck();
-        if(!file_exists($file))
-            throw new FileNotFoundException($file);
-        $dependcheck->depend('mediainfo');
-
-        $process = new Process(array('mediainfo', $file));
-        $process->run();
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-        $info = $process->getOutput();
-		//$info=shell_exec($cmd="mediainfo \"$path\" 2>&1");
-		$info=preg_replace("/Complete name.+\n/",'',$info);
-		$info=preg_replace("/Unique ID.+\n/",'',$info);
-		return $info;
-	}
 }
---
+++ src/Mediainfo.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace datagutten\descriptionMaker;
+
+use dependcheck;
+use DependencyFailedException;
+use FileNotFoundException;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class Mediainfo
+{
+	/**
+	 * @throws DependencyFailedException
+	 */
+	public static function dependcheck()
+	{
+		$dependcheck = new dependcheck();
+		$dependcheck->depend('mediainfo');
+	}
+
+	/**
+	 * @param string $file File name
+	 * @param array $args
+	 * @return Process
+	 * @throws DependencyFailedException|FileNotFoundException
+	 */
+	protected static function mediainfo(string $file, array $args = []): Process
+	{
+		if (!file_exists($file))
+			throw new FileNotFoundException($file);
+		self::dependcheck();
+
+		$process = new Process(array_merge(['mediainfo', $file], $args));
+		$process->run();
+		if (!$process->isSuccessful())
+			throw new ProcessFailedException($process);
+		return $process;
+	}
+
+	/**
+	 * Run mediainfo with plain output, but file name removed
+	 * @param string $file
+	 * @return string
+	 * @throws DependencyFailedException
+	 * @throws FileNotFoundException
+	 */
+	public static function plain(string $file): string
+	{
+		if (!file_exists($file))
+			throw new FileNotFoundException($file);
+		self::dependcheck();
+
+		$process = self::mediainfo($file);
+
+		$info = $process->getOutput();
+		$info = preg_replace("/Complete name.+\n/", '', $info);
+		return preg_replace("/Unique ID.+\n/", '', $info);
+	}
+
+	/**
+	 * Run mediainfo and format output with spaces and BBCode
+	 * @param string $file
+	 * @return string
+	 * @throws DependencyFailedException
+	 * @throws FileNotFoundException
+	 */
+	public static function pretty(string $file): string
+	{
+		$process = self::mediainfo($file, ['--Output=XML']);
+		$xml = simplexml_load_string($process->getOutput());
+		$key_lengths = [];
+		$output = [];
+		foreach ($xml->{'media'}->{'track'} as $track)
+		{
+			$output[] = $track->attributes()->{'type'};
+			//$output[]=$data['@attributes']['type'];
+			$outputkeys[] = 'header';
+			foreach ($track as $key => $value)
+			{
+				if (array_search($key, array('@attributes', 'Unique_ID', 'Complete_name', 'Encoding_settings', 'Color_primaries', 'Transfer_characteristics', 'Matrix_coefficients')) === false)
+				{
+					$output[] = $value;
+					$outputkeys[] = $key;
+					$key_lengths[] = strlen($key);
+				}
+			}
+		}
+
+		$maxlen = max($key_lengths); //Find the longest key
+		$mediainfo = '';
+		foreach ($output as $key => $value)
+		{
+			if ($outputkeys[$key] != 'header')
+				$mediainfo .= str_pad($outputkeys[$key], $maxlen + 5) . ": $value\n";
+			else
+				$mediainfo .= "\n[b]" . $value . "[/b]\n";
+		}
+		return $mediainfo;
+	}
+}
