746435ca5c4d73daaf4ccdb4c4df1efb7590b283
datagutten <datagutten@datagutten.net>
2022-11-05 17:51:45 +0100

    Allow TVDB language selection

--- src/TVDB.php
+++ src/TVDB.php
@@ -11,13 +11,16 @@ class TVDB
     private TVDBScrape $tvdb_scrape;
 	public string $language;
 
-    public function __construct($language = 'eng')
+    public function __construct($language = 'eng', TVDBScrape $tvdb = null)
     {
         $this->language = $language;
-        $this->tvdb_scrape = new TVDBScrape();
+        if (empty($tvdb))
+            $this->tvdb_scrape = new TVDBScrape();
+        else
+            $this->tvdb_scrape = $tvdb;
     }
 
-    function episode_list(string $tvdb_series_slug, int $season_number, string $ordering='official'): string
+    function episode_list(string $tvdb_series_slug, int $season_number, string $ordering='official'): string //TODO: Change argument to season object
     {
         $tvdb_episodes = $this->tvdb_scrape->episodes($tvdb_series_slug, $ordering, true, $season_number);
         $description = '';
@@ -30,12 +33,12 @@ class TVDB
         return $description;
     }
 
-    function episode(string $tvdb_series_slug, int $season_number, int $episode_number): string
+    function episode(string $tvdb_series_slug, int $season_number, int $episode_number): string //TODO: Change argument to episode object
     {
 		$series_obj = $this->tvdb_scrape->series($tvdb_series_slug, $this->language);
 		$season_obj = $series_obj->season($season_number);
 		$episode_obj = $season_obj->episode($episode_number);
-		
+
 		$title = $episode_obj->episode_name();
 		$description = BBCode::link($episode_obj->url(), $title) . "\n";
 
