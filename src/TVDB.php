814f6242c5dbe272f7cc85e45520720a16ae59c1
datagutten <datagutten@datagutten.net>
2022-12-17 12:31:04 +0100

    Use TVDB objects as arguments

--- src/TVDB.php
+++ src/TVDB.php
@@ -1,30 +1,15 @@
 <?php
 
 namespace datagutten\descriptionMaker;
-use datagutten\tvdb\TVDBScrape;
+use datagutten\tvdb\objects\Episode;
+use datagutten\tvdb\objects\Season;
 
 class TVDB
 {
-    /**
-     * @var TVDBScrape
-     */
-    private TVDBScrape $tvdb_scrape;
-	public string $language;
-
-    public function __construct($language = 'eng', TVDBScrape $tvdb = null)
+    function episode_list(Season $season): string
     {
-        $this->language = $language;
-        if (empty($tvdb))
-            $this->tvdb_scrape = new TVDBScrape();
-        else
-            $this->tvdb_scrape = $tvdb;
-    }
-
-    function episode_list(string $tvdb_series_slug, int $season_number, string $ordering='official'): string //TODO: Change argument to season object
-    {
-        $tvdb_episodes = $this->tvdb_scrape->episodes($tvdb_series_slug, $ordering, true, $season_number);
         $description = '';
-        foreach ($tvdb_episodes as $id=>$episode)
+        foreach ($season->episodes(true) as $id=>$episode)
         {
             $description .= BBCode::link($episode->url(), $episode->episode_title()) . "\n";
             if (!empty($episode->description))
@@ -33,17 +18,13 @@ class TVDB
         return $description;
     }
 
-    function episode(string $tvdb_series_slug, int $season_number, int $episode_number): string //TODO: Change argument to episode object
+    function episode(Episode $episode): string
     {
-		$series_obj = $this->tvdb_scrape->series($tvdb_series_slug, $this->language);
-		$season_obj = $series_obj->season($season_number);
-		$episode_obj = $season_obj->episode($episode_number);
+		$title = $episode->episode_name();
+		$description = BBCode::link($episode->url(), $title) . "\n";
 
-		$title = $episode_obj->episode_name();
-		$description = BBCode::link($episode_obj->url(), $title) . "\n";
-
-        if(!empty($episode_obj->description))
-            $description .= $episode_obj->description;
+        if(!empty($episode->description))
+            $description .= $episode->description;
         return $description;
     }
 }
